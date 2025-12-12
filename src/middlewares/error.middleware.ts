4bac868477327f7733e12ac44d1c4a051372d0fd
Oluwaseun Odueso <seunoduez@gmail.com>
2025-12-12 14:08:15 +0000

    chore: files modified

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -29,7 +29,6 @@ const errorMiddleware = (
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'Internal Server Error';
 
-  // Log error
   logger.error({
     message: err.message,
     statusCode: err.statusCode,
