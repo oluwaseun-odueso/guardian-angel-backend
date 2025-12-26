de2b77c3743dc60e1caf1c8d60b14836799132f2
Oluwaseun Odueso <seunoduez@gmail.com>
2025-12-26 14:21:41 +0000

    fix: cors error fix

--- src/app.ts
+++ src/app.ts
@@ -45,30 +45,34 @@ class Application {
   private initializeMiddleware(): void {
     this.app.use(helmet());
 
-    const allowedOrigins = [
-      config.clientUrl,
-      'http://localhost:3000',
-      'http://localhost:5173',  // Vite default
-      'https://panic-five.vercel.app/',
-    ];
+    // const allowedOrigins = [
+    //   config.clientUrl,
+    //   'http://localhost:3000',
+    //   'http://localhost:5173',  // Vite default
+    //   'https://panic-five.vercel.app/',
+    // ];
   
     
     this.app.use(cors({
     //   origin: config.clientUrl,
     //   credentials: true,
     // }));
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
+
+    origin: '*',
+    credentials: true
+
+    // origin: (origin, callback) => {
+    //   // Allow requests with no origin (like mobile apps or curl requests)
+    //   if (!origin) return callback(null, true);
       
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-      },
-      credentials: true,
-      exposedHeaders: ['set-cookie'], // If you're setting cookies
+    //   if (allowedOrigins.indexOf(origin) === -1) {
+    //     const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+    //     return callback(new Error(msg), false);
+    //   }
+    //   return callback(null, true);
+    //   },
+    //   credentials: true,
+    //   exposedHeaders: ['set-cookie'], // If you're setting cookies
     }));
     
     this.app.use(morgan('combined', {
