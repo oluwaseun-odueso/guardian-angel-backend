d52fb43177913c8fd940156af2a8f2a4b21017fb
Oluwaseun Odueso <seunoduez@gmail.com>
2026-01-12 01:24:46 +0000

    refactor: refactored auth for users and responders

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,14 +1,55 @@
-import { Request } from 'express';
+// import { Request } from 'express';
+// import { IUser } from '../models/User.model';
+// import { IResponder } from '../models/Responder.model';
+
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       user?: IUser;
+//     }
+//   }
+// }
+
+// export interface AuthRequest extends Request {
+//   user: IUser;
+// }
+
+
+// types/express.d.ts
 import { IUser } from '../models/User.model';
+import { IResponder } from '../models/Responder.model';
 
 declare global {
   namespace Express {
     interface Request {
+      // For user authentication
       user?: IUser;
+      // For responder authentication  
+      responder?: IResponder;
+      // Common fields
+      userId?: string;
+      responderId?: string;
+      userType?: 'user' | 'responder';
     }
   }
 }
 
-export interface AuthRequest extends Request {
+// Base request interface for controllers
+export interface BaseRequest extends Request {
+  user?: IUser;
+  responder?: IResponder;
+  userId?: string;
+  responderId?: string;
+  userType?: 'user' | 'responder';
+}
+
+// Specific interfaces for type safety
+export interface UserRequest extends BaseRequest {
   user: IUser;
+  userType: 'user';
+}
+
+export interface ResponderRequest extends BaseRequest {
+  responder: IResponder;
+  userType: 'responder';
 }
