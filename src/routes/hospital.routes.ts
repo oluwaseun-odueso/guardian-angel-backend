662bc374934c0158ceb0aa800872e7d2c5ef6e77
Oluwaseun Odueso <seunoduez@gmail.com>
2026-01-12 23:31:31 +0000

    chore: exposed hospital endpoint

--- src/routes/hospital.routes.ts
+++ src/routes/hospital.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import HospitalController from '../controllers/hospital.controller';
-import { authenticate } from '../middlewares/auth.middleware';
+// import { authenticate } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 // Protected routes
-router.get('/', authenticate, HospitalController.fetchAllHospitals); 
+router.get('/', HospitalController.fetchAllHospitals); 
 
 export default router;
